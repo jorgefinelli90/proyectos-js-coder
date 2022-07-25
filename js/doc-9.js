35c7fecf1131b82743fd722dc789fb4c0c6ec92c
Jorge Finelli <jorgefinelli90@gmail.com>
2022-07-25 10:04:35 -0300

    sweet alert

--- js/doc-9.js
+++ js/doc-9.js
@@ -169,6 +169,28 @@ function renderizarCarrito() {
                     onClick: function(){} // Callback after click
                   }).showToast();
                 }
+
+                // operadores ternarios
+
+                carrito.length === 0 && carritoVacio();
+
+
+
+                function carritoVacio(id){
+                    Toastify({
+                        text: `El Carrito se encuentra vacio`,
+                        duration: 2000,
+                        newWindow: false,
+                        close: false,
+                        gravity: "top", // `top` or `bottom`
+                        position: "right", // `left`, `center` or `right`
+                        stopOnFocus: true, // Prevents dismissing of toast on hover
+                        style: {
+                          background: "linear-gradient(to right, #00b09b, #96c93d)",
+                        },
+                        onClick: function(){} // Callback after click
+                      }).showToast();
+                    }
             
             function AgregadoAlCarritoresumen(id){
                 Swal.fire({
