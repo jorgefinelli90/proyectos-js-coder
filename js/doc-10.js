f05475f7759b3cd5bf046189b736c5df4d64b69b
Jorge Finelli <jorgefinelli90@gmail.com>
2022-07-29 12:23:14 -0300

    .

--- js/doc-10.js
+++ js/doc-10.js
@@ -184,24 +184,25 @@ function renderizarCarrito() {
                     duration: 2000,
                   })
                 }
+/* api de contacto, aplicando EMAILJS */
 
-                const btn = document.getElementById('button');
+const btn = document.getElementById('button');
 
-                document.getElementById('form')
-                 .addEventListener('submit', function(event) {
-                   event.preventDefault();
-                
-                   btn.value = 'Sending...';
-                
-                   const serviceID = 'default_service';
-                   const templateID = 'template_31enxt1';
-                
-                   emailjs.sendForm(serviceID, templateID, this)
-                    .then(() => {
-                      btn.value = 'Send Email';
-                      alert('Sent!');
-                    }, (err) => {
-                      btn.value = 'Send Email';
-                      alert(JSON.stringify(err));
-                    });
-                });
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_7c227kd';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
